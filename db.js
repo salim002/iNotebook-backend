b3e2ecdbd3d8082edf98d8ad529cdbadafd027d4
MD SALIM <mdsalim.iiitr@gmail.com>
2023-04-15 23:44:34 +0530

    updated mongo url

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://0.0.0.0:27017/inotebook"
-
+const mongoURI = "mongodb+srv://salim_md:mdsalim@cluster0.5lx7gov.mongodb.net/iNotebook?retryWrites=true&w=majority"
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
         console.log("Connected to mongo successfully");
